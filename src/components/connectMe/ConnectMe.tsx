c3c8c4315a25cdfb1b3558a05c5811da7fb44699
qwertyGhoulzxc <pos2.ebasha@gmail.com>
2023-10-26 19:49:30 +0300

    change links

--- src/components/connectMe/ConnectMe.tsx
+++ src/components/connectMe/ConnectMe.tsx
@@ -4,6 +4,10 @@ import './connectMe.style.scss'
 import {Networks} from './data'
 
 const ConnectMe: FC = () => {
+    const handleClick=(link:string)=>{
+        window.location.href=link;
+    }
+
     return <section className={'ConnectMe'}>
         <div className="cent">
         <Title text={"links"}/>
@@ -11,7 +15,7 @@ const ConnectMe: FC = () => {
 
         <div className="connectMe-container">
             {Networks.map((i,ii)=>{
-                return <div key={ii} className={'card-links'}>{i.component}<a href={i.link} className={'desc'}>{i.desc}</a></div>
+                return <div onClick={()=>{handleClick(i.link)}} key={ii} className={'card-links'}>{i.component}<a href={i.link} className={'desc'}>{i.desc}</a></div>
             })
             }
         </div>
